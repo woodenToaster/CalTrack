6a1c4e80348c0e5ce814055b5aa864b91683a01e
Chris Hogan <litmusik@yahoo.com>
2016-07-05 16:27:32 -0500

    Database connected

---
+++ caltrack.py
@@ -0,0 +1,62 @@
+import os
+import sqlite3
+
+from flask import Flask, g
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+app.config.update({
+    'DATABASE': os.path.join(app.root_path, 'caltrack.db'),
+    'SECRET_KEY': 'development key',
+    'USERNAME': 'admin',
+    'PASSWORD': 'default'
+})
+
+app.config.from_envvar('CALTRACK_SETTINGS', silent=True)
+
+
+def connect_db():
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+
+
+def init_db():
+    db = get_db()
+    with app.open_resource('schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+
+
+@app.cli.command('initdb')
+def initdb_command():
+    """Initializes the database."""
+    init_db()
+    print("Initialized the database.")
+
+
+def get_db():
+    """Opens a new database connnection if there is none yet for the current
+    application context.
+    """
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the database again at the end of the request."""
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
+
+@app.route('/')
+def hello_caltrack():
+    return 'Hello CalTrack!'
+
+
+@app.route('/add/<ingredient>')
+def add_ingredient(ingredient):
+    return "You added " + ingredient
---
+++ schema.sql
@@ -0,0 +1,10 @@
+drop table if exists ingredients;
+create table ingredients (
+  id integer primary key autoincrement,
+  name text not null,
+  calories integer not null,
+  protein integer not null,
+  carbs integer not null,
+  fat integer not null,
+  fiber integer not null
+);
