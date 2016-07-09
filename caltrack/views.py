4799273b704627fec9308e82fd7c8ca660b31b17
Chris Hogan <litmusik@yahoo.com>
2016-07-09 16:39:51 -0500

    Restructuring

---
+++ caltrack/views.py
@@ -0,0 +1,15 @@
+from flask import render_template, flash, redirect
+from caltrack import app
+from .forms import LoginForm
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return "Hello, World!"
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
