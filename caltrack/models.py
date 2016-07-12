8905cb58ff43a2768167ed4a20edb47d66c81287
Chris Hogan <litmusik@yahoo.com>
2016-07-11 21:03:22 -0500

    Login imporevements

--- caltrack/models.py
+++ caltrack/models.py
@@ -6,6 +6,24 @@ class User(db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
 
+    @property
+    def is_authenticated(self):
+        return True
+
+    @property
+    def is_active(self):
+        return True
+
+    @property
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        try:
+            return unicode(self.id)
+        except NameError:
+            return str(self.id)
+
     def __repr__(self):
         return "<User {}>".format(self.username)
 
--- caltrack/views.py
+++ caltrack/views.py
@@ -1,6 +1,8 @@
-from flask import render_template, flash, redirect
-from caltrack import app
+from flask import render_template, flash, redirect, session, url_for, request, g
+from flask_login import login_user, logout_user, current_user, login_required
+from caltrack import app, db, lm, oid
 from .forms import LoginForm
+from .models import User
 
 
 @app.route('/')
@@ -10,11 +12,21 @@ def index():
 
 
 @app.route('/login', methods=['GET', 'POST'])
+@oid.loginhandler
 def login():
+    if g.user is not None and g.user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash_fmt = 'Login requested for OpenID="{}", remember_me={}'
-        flash(flash_fmt.format(form.openid.data, str(form.remember_me.data)))
-        return redirect('/index')
+        session['remember_me'] = form.remember_me.data
+        # flash_fmt = 'Login requested for OpenID="{}", remember_me={}'
+        # flash(flash_fmt.format(form.openid.data, str(form.remember_me.data)))
+        return oid.try_login(form.openid.data, ask_for=['username', 'email'])
     return render_template('login.html', title='Sign In', form=form,
                            providers=app.config['OPENID_PROVIDERS'])
+
+
+@lm.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
